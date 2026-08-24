690106f6093f976c180e791294cc92b3b91a8453
Shalabh Suman <alsoshalabh@gmail.com>
2026-08-24 23:49:18 +0530

    Add files via upload

---
+++ mallocc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+    void main(){
+        int n, i, *ptr, sum =0;
+
+        pritnf("Enter No. Of  Elements : ");
+        scanf("%d", &n);
+
+        ptr = (int *)malloc (n*sizeof(int));
+
+        for(i=0; i<n; i++){
+            
+        }
+    }
