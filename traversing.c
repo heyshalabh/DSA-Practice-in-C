11594ae43da96b3aaa4be3e547686c724c284f62
Shalabh Suman <alsoshalabh@gmail.com>
2026-08-24 23:48:50 +0530

    Add files via upload

---
+++ traversing.c
@@ -0,0 +1,13 @@
+// Traversing : visiting every elements.
+
+#include<stdio.h>
+
+int main(){
+    int arr[10] = {10,20,30,40,50,60,70,80,90,100};
+
+    printf("Traversing : ");
+    
+    for(int i =0; i<10; i++){
+        printf("\n%d",arr[i]);
+    }
+}
